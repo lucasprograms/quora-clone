a1d191f4d69be7e157314f577543cc7833ecb9ef
lucas <lucasprograms@gmail.com>
2015-09-21 21:21:17 -0400

    asset precompile thing

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+config.assets.initialize_on_precompile = false
 module QuoraClone
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -24,5 +24,3 @@ module QuoraClone
     config.active_record.raise_in_transactional_callbacks = true
   end
 end
-
-    config.assets.initialize_on_precompile = false
