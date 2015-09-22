2e0f46682ae9ba947da9ca97fe5f7d71d40d11fc
Lucas McCain <lucasprograms@gmail.com>
2015-09-21 22:31:06 -0400

    switched location of precomile DEFENSE

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-# config.assets.initialize_on_precompile = false
+
 
 module QuoraClone
   class Application < Rails::Application
@@ -24,6 +24,6 @@ module QuoraClone
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -14,6 +14,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
   config.serve_static_files = true
+  config.assets.initialize_on_precompile = false
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
